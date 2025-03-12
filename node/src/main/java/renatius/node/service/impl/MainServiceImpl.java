fb1dd546fecd1f1b1296e99e7dffc09f3855f517
Renat Shinkarev <ddjj676dd@gmail.com>
2025-03-12 22:02:00 +0300

    Saving People 2

--- node/src/main/java/renatius/node/service/impl/MainServiceImpl.java
+++ node/src/main/java/renatius/node/service/impl/MainServiceImpl.java
@@ -1,6 +1,8 @@
 package renatius.node.service.impl;
 
 import org.telegram.telegrambots.meta.api.objects.User;
+import renatius.node.dao.AppUserDAO;
+import renatius.node.entity.AppUser;
 import renatius.node.repository.RawDataDAO;
 import renatius.node.entity.RawData;
 import org.springframework.stereotype.Service;
@@ -8,6 +10,8 @@ import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import renatius.node.service.MainService;
 import renatius.node.service.ProducerService;
+
+import static renatius.node.entity.enums.UserState.BASIC_STATE;
 
 
 @Service
@@ -17,17 +21,45 @@ public class MainServiceImpl implements MainService {
 
     private final  ProducerService producerService;
 
+    private final AppUserDAO appUserDAO;
 
-    public MainServiceImpl(RawDataDAO rawDataDAO ,ProducerService producerService) {
+
+    public MainServiceImpl(RawDataDAO rawDataDAO ,ProducerService producerService, AppUserDAO appUserDAO) {
         this.producerService = producerService;
         this.rawDataDAO = rawDataDAO;
+        this.appUserDAO = appUserDAO;
     }
 
     @Override
     public void processTextMessage(Update update) {
         saveRawData(update);
+        var message = update.getMessage();
+        var telegramUser = message.getFrom();
+        var appUser = findOrSaveAppUser(telegramUser);
+
+        var sendMessage = new SendMessage();
+        sendMessage.setChatId(message.getChatId().toString());
+        sendMessage.setText("Я в мейн сервисе");
+        producerService.produceAnswer(sendMessage);
+
     }
 
+    public AppUser findOrSaveAppUser(User telegramUser){
+        AppUser persistentAppUser = appUserDAO.findAppUserByTelegramUserId(telegramUser.getId());
+        if(persistentAppUser == null){
+            AppUser transientAppUser = AppUser.builder()
+                    .telegramUserId(telegramUser.getId())
+                    .username(telegramUser.getUserName())
+                    .firstName(telegramUser.getFirstName())
+                    .lastName(telegramUser.getLastName())
+                    //TODO изменить значение по умолчанию после добавления регистрации
+                    .isActive(true)
+                    .userState(BASIC_STATE)
+                    .build();
+            return appUserDAO.save(transientAppUser);
+        }
+        return persistentAppUser;
+    }
 
     private void saveRawData(Update update) {
         RawData rawData = RawData.builder()
